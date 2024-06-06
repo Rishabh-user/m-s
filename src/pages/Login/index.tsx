1be858a5fd23640d04d5b17f5f7b3a109b34a187
Rishabh-user <Rishabh@targeticon.com>
2024-06-06 13:13:21 +0530

    []

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -11,7 +11,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import QRCode from 'qrcode.react';
 import { truncate } from "fs";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Link,Outlet, useLocation, useNavigate } from "react-router-dom";
+import Logo from '../../assets/images/pages/logo.png'
+import TinySlider from "@/components/Base/TinySlider";
+import Slide1 from '../../assets/images/pages/Login-slider-img1.png'
+import Slide2 from '../../assets/images/pages/Login-slider-img2.png'
+import Slide3 from '../../assets/images/pages/Login-slider-img3.png'
+import Slide4 from '../../assets/images/pages/Login-slider-img4.png'
 
 function Main() {
 
@@ -25,7 +31,15 @@ function Main() {
   const [error, setError] = useState('');
   const [qrCode, setQrCode] = useState('');
 
-  
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/generate-secret/')
+      .then(response => {
+        console.log(response)
+        setSecret(response.data.secret);
+        setQrCode(`otpauth://totp/YourAppName?secret=${response.data.secret}`);
+      })
+      .catch(err => console.error(err));
+  }, []);
 
   const handleLogin = async () => {
     try {
@@ -71,14 +85,10 @@ function Main() {
           ])}
         >
           <div className="relative z-10 flex flex-col justify-center w-full h-full py-2 lg:py-32">
-            <div className="rounded-[0.8rem] w-[55px] h-[55px] border border-primary/30 flex items-center justify-center">
-              <div className="relative flex items-center justify-center w-[50px] rounded-[0.6rem] h-[50px] bg-gradient-to-b from-theme-1/90 to-theme-2/90 bg-white">
-                <div className="w-[26px] h-[26px] relative -rotate-45 [&_div]:bg-white">
-                  <div className="absolute w-[20%] left-0 inset-y-0 my-auto rounded-full opacity-50 h-[75%]"></div>
-                  <div className="absolute w-[20%] inset-0 m-auto h-[120%] rounded-full"></div>
-                  <div className="absolute w-[20%] right-0 inset-y-0 my-auto rounded-full opacity-50 h-[75%]"></div>
-                </div>
-              </div>
+            <div className="border-bottom pb-5">
+              <Link to="/">
+                <img src={Logo} alt="MNS_Logo" width={172} height={96} className="img-fluid m-auto pb-3" />
+              </Link>
             </div>
 
             {showQrCode ? (
@@ -96,12 +106,9 @@ function Main() {
               <div className="mt-10">
                 <div className="text-2xl font-medium">Sign In</div>
                 <div className="mt-2.5 text-slate-600">
-                  Don't have an account?{" "}
-                  <a className="font-medium text-primary" href="">
-                    Sign Up
-                  </a>
+                  Secure Access to Your Account Easily!
                 </div>
-                <Alert
+                {/* <Alert
                   variant="outline-primary"
                   className="flex items-center px-4 py-3 my-7 bg-primary/5 border-primary/20 rounded-[0.6rem] leading-[1.7]"
                 >
@@ -129,20 +136,20 @@ function Main() {
                       </Alert.DismissButton>
                     </>
                   )}
-                </Alert>
+                </Alert> */}
                 <div className="mt-6">
-                  <FormLabel>Email*</FormLabel>
+                  <FormLabel>User Name</FormLabel>
                   <FormInput
                     type="text"
-                    className="block px-4 py-3.5 rounded-[0.6rem] border-slate-300/80"
+                    className="px-4 py-3.5 rounded-[0.6rem] border-color"
                     placeholder={users.fakeUsers()[0].email}
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                   />
-                  <FormLabel className="mt-4">Password*</FormLabel>
+                  <FormLabel className="mt-4">Password</FormLabel>
                   <FormInput
                     type="password"
-                    className="block px-4 py-3.5 rounded-[0.6rem] border-slate-300/80"
+                    className="px-4 py-3.5 rounded-[0.6rem] border-color"
                     placeholder="************"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
@@ -161,32 +168,27 @@ function Main() {
                         Remember me
                       </label>
                     </div>
-                    <a href="">Forgot Password?</a>
                   </div>
                   <div className="mt-5 text-center xl:mt-8 xl:text-left">
                     <Button
-                      variant="primary"
+                      //variant="success"
                       rounded
-                      className="bg-gradient-to-r from-theme-1/70 to-theme-2/70 w-full py-3.5 xl:mr-3"
+                      className="w-full py-3.5 xl:mr-3 btn-primary"
                       onClick={handleLogin}
                     >
-                      Sign In
-                    </Button>
-                    <Button
-                      variant="outline-secondary"
-                      rounded
-                      className="bg-white/70 w-full py-3.5 mt-3"
-                    >
-                      Sign Up
+                      Login
                     </Button>
                   </div>
                 </div>
               </div>
             )}
           </div>
+          <div className="copyright py-3 text-center">
+            <p>� 2024 Marks & Spencer plc (UK)</p>
+          </div>
         </div>
       </div>
-      <div className="fixed container grid w-screen inset-0 h-screen grid-cols-12 lg:max-w-[1550px] 2xl:max-w-[1750px] pl-14 pr-12 xl:px-24">
+      <div className="fixed container grid w-screen inset-0 h-screen grid-cols-12 lg:max-w-[1550px] 2xl:max-w-[1750px] pl-14">
         <div
           className={clsx([
             "relative h-screen col-span-12 lg:col-span-5 2xl:col-span-4 z-20",
@@ -194,66 +196,29 @@ function Main() {
             "before:content-[''] before:hidden before:lg:block before:absolute before:right-0 before:inset-y-0 before:my-6 before:bg-gradient-to-b before:from-white/10 before:to-slate-50/10 before:bg-white/50 before:w-[800%] before:-mr-4 before:rounded-[0_1.2rem_1.2rem_0/0_1.7rem_1.7rem_0]",
           ])}
         ></div>
-        <div
-          className={clsx([
-            "h-full col-span-7 2xl:col-span-8 lg:relative",
-            "before:content-[''] before:absolute before:lg:-ml-10 before:left-0 before:inset-y-0 before:bg-gradient-to-b before:from-theme-1 before:to-theme-2 before:w-screen before:lg:w-[800%]",
-            "after:content-[''] after:absolute after:inset-y-0 after:left-0 after:w-screen after:lg:w-[800%] after:bg-texture-white after:bg-fixed after:bg-center after:lg:bg-[25rem_-25rem] after:bg-no-repeat",
-          ])}
-        >
-          <div className="sticky top-0 z-10 flex-col justify-center hidden h-screen ml-16 lg:flex xl:ml-28 2xl:ml-36">
-            <div className="leading-[1.4] text-[2.6rem] xl:text-5xl font-medium xl:leading-[1.2] text-white">
-              Embrace Excellence <br /> in Dashboard Development
-            </div>
-            <div className="mt-5 text-base leading-relaxed xl:text-lg text-white/70">
-              Unlock the potential of Tailwise, where developers craft
-              meticulously structured, visually stunning dashboards with
-              feature-rich modules. Join us today to shape the future of your
-              application development.
-            </div>
-            <div className="flex flex-col gap-3 mt-10 xl:items-center xl:flex-row">
-              <div className="flex items-center">
-                <div className="w-9 h-9 2xl:w-11 2xl:h-11 image-fit zoom-in">
-                  <Tippy
-                    as="img"
-                    alt="Tailwise - Admin Dashboard Template"
-                    className="rounded-full border-[3px] border-white/50"
-                    src={users.fakeUsers()[0].photo}
-                    content={users.fakeUsers()[0].name}
-                  />
-                </div>
-                <div className="-ml-3 w-9 h-9 2xl:w-11 2xl:h-11 image-fit zoom-in">
-                  <Tippy
-                    as="img"
-                    alt="Tailwise - Admin Dashboard Template"
-                    className="rounded-full border-[3px] border-white/50"
-                    src={users.fakeUsers()[0].photo}
-                    content={users.fakeUsers()[0].name}
-                  />
-                </div>
-                <div className="-ml-3 w-9 h-9 2xl:w-11 2xl:h-11 image-fit zoom-in">
-                  <Tippy
-                    as="img"
-                    alt="Tailwise - Admin Dashboard Template"
-                    className="rounded-full border-[3px] border-white/50"
-                    src={users.fakeUsers()[0].photo}
-                    content={users.fakeUsers()[0].name}
-                  />
-                </div>
-                <div className="-ml-3 w-9 h-9 2xl:w-11 2xl:h-11 image-fit zoom-in">
-                  <Tippy
-                    as="img"
-                    alt="Tailwise - Admin Dashboard Template"
-                    className="rounded-full border-[3px] border-white/50"
-                    src={users.fakeUsers()[0].photo}
-                    content={users.fakeUsers()[0].name}
-                  />
-                </div>
+        <div className="col-span-12 lg:col-span-7 2xl:col-span-8">
+          <div className="h-full flex items-center justify-center relative">
+            <TinySlider
+              options={{
+                controls: false,
+                items: 1,
+                nav: true,
+              }}
+              className="w-full h-full flex h-full"
+            >
+              <div className="h-full">
+                <img src={Slide1} alt="Slide 1" className="w-full h-full" />
               </div>
-              <div className="text-base xl:ml-2 2xl:ml-3 text-white/70">
-                Over 7k+ strong and growing! Your journey begins here.
+              <div className="h-full">
+                <img src={Slide2} alt="Slide 2" className="w-full h-full" />
               </div>
-            </div>
+              <div className="h-full">
+                <img src={Slide3} alt="Slide 3" className="w-full h-full" />
+              </div>
+              <div className="h-full">
+                <img src={Slide4} alt="Slide 4" className="w-full h-full" />
+              </div>
+            </TinySlider>
           </div>
         </div>
       </div>
